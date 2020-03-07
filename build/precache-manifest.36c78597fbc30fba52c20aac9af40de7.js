61a59d00e408c520be17fa28bd1c8eadffb08b0c
Jayachandran <jgajendran@apple.com>
2020-03-07 10:42:46 +0530

    Added bundle files

---
+++ build/precache-manifest.36c78597fbc30fba52c20aac9af40de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1489422a5e04c2aa7e3b5599cf4361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61f4f8f96375118b3ca4",
+    "url": "/static/css/main.a5bd5c60.chunk.css"
+  },
+  {
+    "revision": "71ce5841765bc8269ae5",
+    "url": "/static/js/2.ed548d84.chunk.js"
+  },
+  {
+    "revision": "aa762385ab423a29206547471fcc4bac",
+    "url": "/static/js/2.ed548d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f4f8f96375118b3ca4",
+    "url": "/static/js/main.8c75163a.chunk.js"
+  },
+  {
+    "revision": "f701792ed85e5f895166",
+    "url": "/static/js/runtime-main.cdfb4480.js"
+  }
+]);
